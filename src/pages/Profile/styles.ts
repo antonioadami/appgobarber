bf89467563e08d806a9b86f57319f39c9d3304ce
Antonio Adami <antonioadamipalma@gmail.com>
2021-03-14 12:32:08 -0300

    Página de perfil

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    padding: 80px 30px 40px;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const Title = styled.Text`
+    font-size: 20px;
+    color: #f4ede8;
+    font-family: 'RobotoSlab-Medium';
+    margin: 24px 0;
+    text-align: left;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+    width: 186px;
+    height: 186px;
+    border-radius: 98px;
+    align-self: center;
+`;
